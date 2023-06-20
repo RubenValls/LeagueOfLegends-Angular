1258cced5db7160c8622fdc8cd22c7dc0adc87a0
Rubén Valls <r.vallsaparici@gmail.com>
2023-06-20 12:22:38 +0200

    improved card visualization

--- src/app/components/champion-card/champion-card.component.ts
+++ src/app/components/champion-card/champion-card.component.ts
@@ -12,6 +12,7 @@ export class ChampionCardComponent {
     id: '',
     name: '',
     title: '',
+    blurb: '',
     info: {},
     tags: [],
   }
--- src/app/models/championCard.model.ts
+++ src/app/models/championCard.model.ts
@@ -2,6 +2,7 @@ export interface ChampionCard {
   id: string;
   name: string;
   title: string;
+  blurb: string;
   info: any;
   tags: string[];
 }
