03e88542008475e51793c04eba1bc5ed34c2d571
Rubén Valls <r.vallsaparici@gmail.com>
2023-06-21 13:23:22 +0200

    improved main page

--- src/app/home-component/home-component.component.ts
+++ src/app/home-component/home-component.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { defaultChampions } from 'src/constants/defaultChampions';
 
 @Component({
   selector: 'app-home-component',
@@ -6,7 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./home-component.component.css']
 })
 export class HomeComponentComponent {
+  championSelected = 0
+  type = 'assassin'
+  defaultChampions = defaultChampions
   lolNavigate(){
     window.location.href = 'https://signup.leagueoflegends.com/es-es/signup/index'
+  }
+  changeChampion(number: number, type: string){
+    this.championSelected = number;
+    this.type = type
   }
 }
---
+++ src/constants/defaultChampions.ts
@@ -0,0 +1,32 @@
+export const defaultChampions = [
+  {
+    name: 'Akali',
+    title: 'La Asesina Sigilosa',
+    img: 'https://www.leagueoflegends.com/static/assassin-d64d3ffdda15e1eed637aefe6a2c7fee.png'
+  },
+  {
+    name: 'Yasuo',
+    title: 'La Espada sin Honor',
+    img: 'https://www.leagueoflegends.com/static/fighter-7a08920b696ecdb673edeeae1d3c616e.png'
+  },
+  {
+    name: 'Lux',
+    title: 'La Dama Luminosa',
+    img: 'https://www.leagueoflegends.com/static/mage-3bfa6dfe620adafe5e539c2e470f4acc.png'
+  },
+  {
+    name: 'Jinx',
+    title: 'La Bala Perdida',
+    img: 'https://www.leagueoflegends.com/static/marksman-b339ed8fd7e04ff2c3fca022c5d299fb.png'
+  },
+  {
+    name: 'Thresh',
+    title: 'El carcelero Implacable',
+    img: 'https://www.leagueoflegends.com/static/support-d63ae08baf517425864ddc020a5871d5.png'
+  },
+  {
+    name: 'Leona',
+    title: 'El Amanecer Radiante',
+    img: 'https://www.leagueoflegends.com/static/tank-1245abc326bd98c567ab22659719a1a3.png'
+  },
+]
