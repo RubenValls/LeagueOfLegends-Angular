3250f43c5038ef2b35ed6109e68dbd82a40f1209
Rubén Valls <r.vallsaparici@gmail.com>
2023-06-22 00:50:09 +0200

    added a finder at champions section

--- src/app/components/all-champions/all-champions.component.ts
+++ src/app/components/all-champions/all-champions.component.ts
@@ -16,4 +16,14 @@ export class AllChampionsComponent {
         this.champions = Object.entries(data.data);
       })
   }
+  addFind(filter: string){
+    this.http.get('https://ddragon.leagueoflegends.com/cdn/13.12.1/data/es_ES/champion.json')
+      .subscribe((data: any) => {
+        this.champions = Object.entries(data.data);
+        if(filter){
+          const filteredChamps = this.champions.filter((champion: any) => champion[1].name.toLowerCase().includes(filter.toLowerCase()))
+          this.champions = filteredChamps
+        }
+    })
+  }
 }
